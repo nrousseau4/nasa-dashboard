7bc6eef08c0cf9f60c205c7333f062e28904fbea
Nicholas Rousseau <njr5584@psu.edu>
2024-10-22 13:12:47 -0400

    Update from Class 10/22

--- nasa-dashboard.js
+++ nasa-dashboard.js
@@ -1,17 +1,16 @@
 /**
+ * all main stuff has been added, need to add functions and such
+ */
+
+/**
  * Copyright 2024 nrousseau4
  * @license Apache-2.0, see LICENSE for full text.
  */
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "./nasa-image.js";
 
-/**
- * `nasa-dashboard`
- * 
- * @demo index.html
- * @element nasa-dashboard
- */
 export class nasaDashboard extends DDDSuper(I18NMixin(LitElement)) {
 
   static get tag() {
@@ -20,56 +19,79 @@ export class nasaDashboard extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.title = "";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/nasa-dashboard.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
-    });
-  }
+    this.value = null;
+    this.title = '';
+    this.loading = false;
+    this.items = [];
+  };
 
   // Lit reactive properties
   static get properties() {
     return {
-      ...super.properties,
       title: { type: String },
+      loading: { type: Boolean, reflect: true },
+      items: { type: Array, },
+      value: { type: String },
     };
   }
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
+    return css`
       :host {
         display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+      :host([loading]) .results {
+        opacity: 0.1;
+        visibility: hidden;
+        height: 1px;
       }
-      h3 span {
-        font-size: var(--nasa-dashboard-label-font-size, var(--ddd-font-size-s));
+      .results {
+        visibility: visible;
+        height: 100%;
+        opacity: 1;
+        transition-delay: .5s;
+        transition: .5s all ease-in-out;
       }
-    `];
+
+      details {
+        margin: 16px;
+        padding: 16px;
+        background-color: blue;
+      }
+      summary {
+        font-size: 24px;
+        padding: 8px;
+        color: white;
+        font-size: 42px;
+      }
+      input {
+        font-size: 20px;
+        line-height: 40px;
+        width: 100%;
+      }
+    `;
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+      <h2>${this.title}</h2>
+      <details open>
+        <summary>Search inputs</summary>
+        <div>
+          <input id="input" placeholder="Search NASA images" @input="${this.inputChanged}" />
+        </div>
+      </details>
+      <div class="results">
+        ${this.items.map((item, index) => html`
+        <nasa-image
+          source="${item.links[0].href}"
+          title="${item.data[0].title}"
+        ></nasa-image>
+        `)}
+      </div>
+    `;
   }
 
   /**
